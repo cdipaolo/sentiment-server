6f057a3ca4f314d7ff4c907bda9aed4a4f75f180
Conner DiPaolo <cdipaolo96@gmail.com>
2015-07-30 21:22:17 -0700

    Added fail testing for POST /analyze

--- sentiment_test.go
+++ sentiment_test.go
@@ -70,3 +70,36 @@ func TestSentimentShouldPass1(t *testing.T) {
 		t.Errorf("ERROR: responded individual word sentiment should equal in length the same response from the library!\n\tShould be: %v\n\tReturned: %v\n", should.Words, analysis.Words)
 	}
 }
+
+func TestSentimentShouldFail1(t *testing.T) {
+	text := `The anti-immigration people have to invent some explanation to account for all the effort technology companies have expended trying to make immigration easier. So they claim it's because they want to drive down salaries. But if you talk to startups, you find practically every one over a certain size has gone through legal contortions to get programmers into the US, where they then paid them the same as they'd have paid an American. Why would they go to extra trouble to get programmers for the same price? The only explanation is that they're telling the truth: there are just not enough great programmers to go around`
+	txt := fmt.Sprintf(`{
+		"text": "%v
+	}`, text)
+
+	status, body, err := post("analyze", txt)
+	if err != nil {
+		t.Errorf("ERROR: error trying to post\n\t%v\n", err)
+	}
+	if status != http.StatusBadRequest {
+		t.Errorf("ERROR: status returned should be 400 BAD REQUEST\n\t%v\n", string(body))
+	}
+	if len(body) == 0 {
+		t.Fatalf("ERROR: body should not be nil!\n")
+	}
+}
+
+func TestSentimentShouldFail2(t *testing.T) {
+	txt := ``
+
+	status, body, err := post("analyze", txt)
+	if err != nil {
+		t.Errorf("ERROR: error trying to post\n\t%v\n", err)
+	}
+	if status != http.StatusBadRequest {
+		t.Errorf("ERROR: status returned should be 400 BAD REQUEST\n\t%v\n", string(body))
+	}
+	if len(body) == 0 {
+		t.Fatalf("ERROR: body should not be nil!\n")
+	}
+}
